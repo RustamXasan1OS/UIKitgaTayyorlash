6993e845b0763855f13ae63d9e31b1ee765817af
RutamXasan1OS <rustamxasan1@gmail.com>
2022-11-01 14:46:24 +0500

    UIKit ga tayyorlash

---
+++ UIKitgaTayyorlash/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitgaTayyorlash
+//
+//  Created by RustamXasan on 01/11/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
